12435ee5d64f2642583a0988adb24169ad78ea64
hymanjmaa <hymanjma@gmail.com>
2017-06-23 11:34:17 +0800

    update

--- prettylifeInterface.py
+++ prettylifeInterface.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 import hashlib
 import web
-
+import os
 import receive
 import reply
 
 
 class PrettyLifeInterface:
     def __init__(self):
-        pass
+        self.app_root = os.path.dirname(__file__)
+        self.templates_root = os.path.join(self.app_root, 'templates')
+        self.render = web.template.render(self.templates_root)
 
     def GET(self):
         try:
